e1faa3d0db5a4c8f931c539aac32bc42cf41d833
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-09-27 23:24:55 +0200

    Spellcheck ShareableId

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -51,7 +51,7 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
           type: accountData.type as string,
           subtype: accountData.subtype! as string,
           appwriteItemId: bank.$id,
-          sharableId: bank.shareableId,
+          shareableId: bank.shareableId,
         };
 
         return account;
